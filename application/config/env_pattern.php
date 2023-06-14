b989bbb937db97fb75e8c9a4f7453af135486afd
MuhammadSabri1306 <muhammadsabri1306@gmail.com>
2023-06-14 11:16:45 +0800

    add pue ofline limit time in env pattern

--- application/config/env_pattern.php
+++ application/config/env_pattern.php
@@ -162,6 +162,31 @@ class EnvPattern {
         ];
     }
 
+    public static function getPueOfflineTime($toString = false)
+    {
+        $startTime = new DateTime('now');
+        $startTime->setTime(0, 0, 0);
+
+        $endTime = new DateTime('now');
+        $endTime->setTime(23, 59, 59);
+
+        // ====== First time on this month
+        // $startTime->modify('first day of this month');
+
+        // ====== First time on this year
+        $startTime->modify('first day of January ' . $startTime->format('Y'));
+
+        // ====== Last time on this month
+        $endTime->modify('last day of this month');
+
+        return (object) [
+            'start' => $toString ? $startTime->format('Y-m-d H:i:s') : $startTime->getTimestamp(),
+            'end' => $toString ? $endTime->format('Y-m-d H:i:s') : $endTime->getTimestamp()
+        ];
+    }
+
+    // ================== CODE LAMA ==================
+
     public static function getUpdatableActivityTime($toString = false)
     {
         $startTime = new DateTime('now');
